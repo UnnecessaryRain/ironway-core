1fba7d0735da19f5c61204bb188d22be89a8afed
Samuel-Lewis <mazuel@gmail.com>
2018-04-02 14:45:09 +0930

    Add interpreter for commands to game instance
    Only works for core game instance, not client messages

---
+++ pkg/interpreter/interpreter.go
@@ -0,0 +1,35 @@
+package interpreter
+
+import (
+	"strings"
+
+	"github.com/UnnecessaryRain/ironway-core/pkg/game"
+	"github.com/UnnecessaryRain/ironway-core/pkg/game/commands"
+
+	log "github.com/sirupsen/logrus"
+)
+
+// commandDict list of user commands to relevant function
+var commandDict = map[string]func(string) game.Command{
+	"debug": commands.NewDebug,
+}
+
+// FindCommand interpretes keyword of string and return relevant Command
+func FindCommand(cmd string) game.Command {
+	// Empty command passed somehow
+	if len(cmd) == 0 {
+		log.Warningln("Passed command was empty")
+		return commands.NewNotFound("")
+	}
+
+	// Chat check
+	if cmd[0] != '/' {
+		return commands.NewChat(cmd)
+	}
+
+	key := strings.Fields(cmd)[0][1:]
+	if val, ok := commandDict[key]; ok {
+		return val(cmd)
+	}
+	return commands.NewNotFound(key)
+}
