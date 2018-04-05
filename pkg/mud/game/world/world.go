d5145160acc2d9e652075951b1f257ee80aa78b3
Sam Lewis <mazuel@gmail.com>
2018-04-05 10:00:56 +0930

    Add comments

--- pkg/mud/game/world/world.go
+++ pkg/mud/game/world/world.go
@@ -1,9 +1,11 @@
 package world
 
+// World contains the game map and entities
 type World struct {
 	Atlas *Atlas
 }
 
+// NewWorld creates a new world and popualtes it
 func NewWorld() *World {
 	return &World{
 		Atlas: NewAtlas(),
